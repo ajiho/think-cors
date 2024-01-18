0374efc89fa3229caf993836879307e07449960f
一个喷嚏是思念 <1035936946@qq.com>
2024-01-18 07:15:12 +0000

    update src/middleware/Cors.php. Access-Control-Allow-Origin 的规则 只允许填写一个域名
    所以更改为数组匹配进行填写
    
    Signed-off-by: 一个喷嚏是思念 <1035936946@qq.com>

--- src/middleware/Cors.php
+++ src/middleware/Cors.php
@@ -8,8 +8,12 @@ class Cors
 {
     public function handle($request, \Closure $next)
     {
-
-        header("Access-Control-Allow-Origin:" . implode(',', Config::get('cors.allow_origin')));
+        $Origin = in_array('*',Config::get('cors.allow_origin'))?'*':'';
+        if (in_array($request->server('HTTP_ORIGIN'), Config::get('cors.allow_origin'))) {
+            $Origin = $request->server('HTTP_ORIGIN');
+        }
+        header('Access-Control-Allow-Origin:'.$Origin);
+        header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Allow-Headers:" . implode(',', Config::get('cors.allow_headers')));
         header("Access-Control-Allow-Methods:" . implode(',', Config::get('cors.allow_methods')));
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
